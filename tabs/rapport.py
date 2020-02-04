b267c1434ae09cce221d19ec72804bb09de313d4
marque-antoine <dannerma@gmail.com>
2020-02-04 18:50:39 -0500

    version plus realiste mdr

--- tabs/rapport.py
+++ tabs/rapport.py
@@ -1,4 +1,3 @@
-import plotly.express as px
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -11,10 +10,6 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
-
-# This dataframe has 244 lines, but 4 distinct values for `day`
-df = px.data.tips()
-fig = px.pie(df, values='tip', names='day')
 
 
 def generate_table(dataframe, max_rows=10):
@@ -31,5 +26,5 @@ def generate_table(dataframe, max_rows=10):
 
 rapport_layout = html.Div(children=[
     html.H4(children='US Agriculture Exports (2011)'),
-    fig.show()
+    generate_table(df)
 ])
